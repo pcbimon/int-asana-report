a8e509d46d6354fa60ae9fe72f4508d68fcb2f22
Patipat Chewprecha <patipat.che@mahidol.edu>
2025-09-20 20:39:50 +0700

    feat: enhance CurrentTasksTable with badge and pagination components

--- web/components/CurrentTasksTable.tsx
+++ web/components/CurrentTasksTable.tsx
@@ -6,12 +6,20 @@ import { BsExclamationTriangle } from "react-icons/bs";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Badge } from "./ui/badge";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./ui/pagination";
 export default function CurrentTasksTable() {
   return (
     <div className="my-4">
@@ -39,16 +47,115 @@ export default function CurrentTasksTable() {
           <Table>
             <TableHeader>
               <TableRow>
-                <TableHead className="">Task</TableHead>
-                <TableHead>Created</TableHead>
-                <TableHead>Due Date</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Type</TableHead>
+                <TableHead className="w-1/2">Task</TableHead>
+                <TableHead className="w-1/6">Created</TableHead>
+                <TableHead className="w-1/6">Due Date</TableHead>
+                <TableHead className="w-1/6">Status</TableHead>
+                <TableHead className="w-1/6">Type</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">
+                  <div className="flex flex-col">
+                    <span className="font-medium">Design new UI</span>
+                  </div>
+                </TableCell>
+                <TableCell>01 Oct 2025</TableCell>
+                <TableCell>15 Oct 2023</TableCell>
+                <TableCell>
+                  <Badge variant="success">
+                    <IoMdCheckmarkCircleOutline /> Completed
+                  </Badge>
+                </TableCell>
+                <TableCell>
+                  <Badge variant="default">Owner</Badge>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <div className="flex flex-col">
+                    <span className="font-medium">
+                      Implement authentication
+                    </span>
+                    <span className="text-xs text-gray-500">
+                      Mr. Smith, Mr. Johnson
+                    </span>
+                  </div>
+                </TableCell>
+                <TableCell>01 Oct 2025</TableCell>
+                <TableCell>15 Oct 2023</TableCell>
+                <TableCell>
+                  <Badge variant="success">
+                    <IoMdCheckmarkCircleOutline /> Completed
+                  </Badge>
+                </TableCell>
+                <TableCell>
+                  <Badge variant="secondary">Collaborator</Badge>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <div className="flex flex-col">
+                    <span className="font-medium">Implement function A</span>
+                  </div>
+                </TableCell>
+                <TableCell>01 Oct 2025</TableCell>
+                <TableCell>15 Oct 2023</TableCell>
+                <TableCell>
+                  <Badge variant="secondary">
+                    <MdOutlineAccessTime /> Pending
+                  </Badge>
+                </TableCell>
+                <TableCell>
+                  <Badge variant="default">Owner</Badge>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <div className="flex flex-col">
+                    <span className="font-medium">Implement function B</span>
+                  </div>
+                </TableCell>
+                <TableCell>01 Oct 2025</TableCell>
+                <TableCell>15 Oct 2023</TableCell>
+                <TableCell>
+                  <Badge variant="destructive">
+                    <BsExclamationTriangle /> Overdue
+                  </Badge>
+                </TableCell>
+                <TableCell>
+                  <Badge variant="default">Owner</Badge>
+                </TableCell>
+              </TableRow>
             </TableBody>
           </Table>
+          <div className="flex justify-between mt-4">
+            <div className="text-sm text-gray-500">
+              Showing 1 to 4 of 56 entries
+            </div>
+            <div className="flex items-center">
+              <Pagination>
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious href="#" />
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#" isActive={true}>1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">2</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">3</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationNext href="#" />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+            </div>
+          </div>
         </CardContent>
       </Card>
     </div>
