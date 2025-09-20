cba5f620b5a0ba9902a74729d5f168ab8c828b6d
Patipat Chewprecha <patipat.che@mahidol.edu>
2025-09-20 14:44:52 +0700

    feat: implement dashboard page with summary metrics and export options

---
+++ web/app/dashboard/[assignee-gid]/page.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { MdLogout } from "react-icons/md";
+import { FaFileExcel, FaFilePdf } from "react-icons/fa";
+import React from "react";
+import "../../globals.css";
+import SummaryMetricCard from "@/components/SummaryMatricCard";
+export default function DashboardPage() {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <div className="container mx-auto">
+        <Card>
+          <CardContent>
+            <div className="flex">
+              <div className="flex flex-col">
+                <h1 className="text-2xl font-bold">Dashboard Overview</h1>
+                <h1 className="text-xl font-light">Patipat Chewprecha</h1>
+              </div>
+              <div className="flex-grow text-right space-x-2">
+                <Button variant="outline">
+                  <FaFilePdf /> Export PDF
+                </Button>
+                <Button variant="outline">
+                  <FaFileExcel /> Export Excel
+                </Button>
+                <Button variant="default">
+                  <MdLogout /> Logout
+                </Button>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+        <SummaryMetricCard />
+      </div>
+    </div>
+  );
+}
---
+++ web/components/SummaryMatricCard.tsx
@@ -0,0 +1,82 @@
+import { FiTarget } from "react-icons/fi";
+import { IoMdTrendingUp } from "react-icons/io";
+import { BsExclamationTriangle } from "react-icons/bs";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+export default function SummaryMetricCard() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-8">
+      <Card>
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>Total Tasks</CardTitle>
+            <div className="w-8 h-8 rounded-full bg-blue-50 flex items-center justify-center">
+              <FiTarget className="text-blue-500" />
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className="text-3xl font-bold">42</div>
+          <CardDescription className="text-xs text-gray-500">
+            Number of tasks assigned
+          </CardDescription>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>Completed</CardTitle>
+            <div className="w-8 h-8 rounded-full bg-green-50 flex items-center justify-center">
+              <FiTarget className="text-green-500" />
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className="text-3xl font-bold">42</div>
+          <CardDescription className="text-xs text-gray-500">
+            Completed subtasks
+          </CardDescription>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>Completetion Rate</CardTitle>
+            <div className="w-8 h-8 rounded-full bg-green-50 flex items-center justify-center">
+              <IoMdTrendingUp className="text-green-500" />
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className="text-3xl font-bold">100%</div>
+          <CardDescription className="text-xs text-gray-500">
+            Percentage completed
+          </CardDescription>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>Overdue</CardTitle>
+            <div className="w-8 h-8 rounded-full bg-gray-50 flex items-center justify-center">
+              <BsExclamationTriangle className="text-gray-500" />
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className="text-3xl font-bold">0</div>
+          <CardDescription className="text-xs text-gray-500">
+            Overdue subtasks
+          </CardDescription>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
